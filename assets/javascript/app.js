01a0a55a2ce9f7666115c81a408838ab746e75e6
Austin Tsiaperas <liketexas@gmail.com>
2018-01-10 21:37:52 -0600

    added click event with tests, it's working!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,40 +17,7 @@ function Timer(length,game,number){
         this.timeUp = false;
         this.active = false;
         this.time = length;
-        this.start = function(){
-             if(!this.active)
-            {
-                console.log(this);
-                this.intervalId = setInterval(function(this){ 
-                game.draw(game.questions[number]);
-                $("#seconds").text(this.time);
-            
-                if (this.time>0)
-                {
-                    this.time--;
-                }
-                else
-                {
-                this.timeUp = true;
-                this.stop();
-                }
-                },1000);
-                this.active = true;
-            }
-        }
-        this.stop = function(){
-            if(this.active)
-            {   
-                clearInterval(this.intervalId);
-                this.active = false;
-            }
-        }
-                   
-        this.reset = function(){
-            this.timeUp = false;
-            this.active = false;
-            this.time = length;
-        }
+        this.number = number;
 }
 
 var questions = [];
@@ -65,14 +32,63 @@ function Question(text,a,b,c,d,answer)
 	this.d = d;
     this.answer = answer;
 }
-var question = new Question("What is my name?","Austin","Steve","Jack","Fish","Austin");
+var question = new Question("What is my name?","Austin","Steve","Jack","Fish","a");
 questions.push(question);
 
 $(document).ready(function(){
 var interval;
 var newGame = new Game(questions);
 var timer = new Timer(30,newGame,0);
+Timer.prototype.start = function(){
+            
+            if(!timer.active)
+            {                
+                timer.intervalId = setInterval(timer.count,1000);
+                timer.active = true;
+            }
+        };
+Timer.prototype.count = function(){
+            newGame.draw(newGame.questions[timer.number]);
+            $("#seconds").text(timer.time);
+            console.log(timer.time);
+
+            if (timer.time>0)
+            {
+                timer.time--;
+            }
+            else
+            {
+                timer.timeUp = true;
+                timer.stop();
+            }
+        };
+
+Timer.prototype.reset = function(){
+            timer.timeUp = false;
+            timer.active = false;
+            timer.time = length;
+        };
+Timer.prototype.stop = function(){
+            if(timer.active)
+            {   
+                clearInterval(timer.intervalId);
+                timer.active = false;
+            }
+        };
 timer.start();
 
+$(".answer").on("click", function(){
+    var choice = $(this).attr("id");
+    console.log(choice);
+    if (choice != questions[timer.number].answer)
+    {
+       alert("wrong!");
+    }
+    else
+    {
+       alert("right!");
+    }
 
+})
 });
+
